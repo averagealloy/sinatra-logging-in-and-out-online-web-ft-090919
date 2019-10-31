e9590c7ef72420501f59a5f5fb8eeea226ce2e26
Mike McTighe <m.mctighe10@gmail.com>
2019-10-31 00:49:01 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -4,8 +4,9 @@ class Helpers
     @user = User.find_by_id(session[:user_id])
   end
   def self.is_logged_in?(session)
-     session[:user_id]
-
+     if session[:user_id]
+       true
+     end 
 
     end
 
